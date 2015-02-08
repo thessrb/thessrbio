4aa094461986b601fce3b3abe4d9b309c74f81a4
Steve Laskaridis <stevelaskaridis@gmail.com>
2015-02-08 19:08:59 +0200

    fixed typo

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -6,7 +6,7 @@ class PresentationsController < ApplicationController
 			@presentations = @presentations.sort {|ppt1, ppt2| ppt1.title <=> ppt2.title}
 		elsif params[:sort_by] == 'date'
 			@presentations = @presentations.sort {|ppt1, ppt2| ppt1.date_presented <=> ppt2.date_presented}
-		elsif params[:sort_by] == 'presentor'
+		elsif params[:sort_by] == 'presenter'
 			@presentations = @presentations.sort {|ppt1, ppt2| ppt1.presenter <=> ppt2.presenter}
 		end
 	end
